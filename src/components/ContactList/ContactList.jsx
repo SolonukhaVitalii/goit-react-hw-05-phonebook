1cc3acdab3f4ed014a8ff73b49891019e2adefeb
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2021-02-15 11:51:25 +0200

    DZ5 animation

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import s from './ContactList.module.css';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 
 const ContactList = ({ contacts, onDeleteContact}) => (
-    <ul className={s.list}>
+    <TransitionGroup component="ul" className={s.list}>
         {contacts.map(({ id, name, number }) => (
+            <CSSTransition key={id} timeout={250} classNames={s}>
             <li key={id} className={s.item}>
                 <p className={s.name}>{name}:</p>
                 <p className={s.number}>{number}</p>
                 <button className={s.button} onClick={(() => onDeleteContact(id))}>Удалить</button>
             </li>
+            </CSSTransition>
         ))}
-    </ul>
+    </TransitionGroup>
 );
 
 ContactList.propTypes = {
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import s from './Filter.module.css';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 
 const Filter = ({ value, onChange }) => (
+    <CSSTransition in={true} timeout={250} classNames={s} unmountOnExit>
     <label htmlFor="">Find contacts by name
         <input className={s.input}
             type="text"
@@ -10,6 +12,7 @@ const Filter = ({ value, onChange }) => (
             onChange={onChange}
         />
     </label>
+    </CSSTransition>
 );
 
 Filter.propTypes = {
